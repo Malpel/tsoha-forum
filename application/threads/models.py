64f25c66d8a3c38e4ffd21a4ee839791a020cbfc
malpel <m.a.pelkonen2@gmail.com>
2019-01-03 19:51:16 +0200

    korjattu resurssien suojausta, pieniä muutoksia

--- application/threads/models.py
+++ application/threads/models.py
@@ -16,8 +16,29 @@ class Thread(db.Model):
     title = db.Column(db.String(50), nullable=False)
     comments = db.relationship("Comment", backref='comment', lazy=True)
 
+
     def __init__(self, title):
         self.title = title
+   
+    def get_id(self):
+        return self.id
+
+    def comment_thread(self):
+        
+        stmt = text("WITH RECURSIVE cte(id, parent_id, account, text, date_created, date_modifed, root, level)" 
+        " AS (SELECT comment.id, comment.parent_id, comment.user, comment.text, comment.date_created, comment.date_modifed, ARRAY[comment.id], 0"  
+        " FROM comment WHERE comment.thread_id = :thread_id AND parent_id IS NULL " 
+        " UNION ALL SELECT comment.id, comment.parent_id, comment.user, comment.text, comment.date_created, comment.date_modifed, root || ARRAY[comment.id], cte.level + 1" 
+        " FROM cte"
+        " INNER JOIN comment ON comment.parent_id = cte.id)"
+        " SELECT * FROM cte ORDER BY root").params(thread_id=self.id)
+
+        res = db.engine.execute(stmt)
+        threaded = []
+        for row in res:
+            threaded.append(row)
+            
+        return threaded
 
 
 class Comment(db.Model):
@@ -31,28 +52,15 @@ class Comment(db.Model):
     text = db.Column(db.String(1000), nullable=False)
     parent_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
     thread_id = db.Column(db.ForeignKey("thread.id"))
+    is_deleted = db.Column(db.Boolean, default=False)
     parent = db.relationship("Comment", backref="children", remote_side=[id])
 
     def __init__(self, text):
         self.text = text
         
-
-    def comment_thread(thread_id):
-        
-        stmt = text("WITH RECURSIVE cte(id, parent_id, account, text, date_created, date_modifed, root, level)" 
-        " AS (SELECT comment.id, comment.parent_id, comment.user, comment.text, comment.date_created, comment.date_modifed, ARRAY[comment.id], 0"  
-        " FROM comment WHERE comment.thread_id = :thread_id AND parent_id IS NULL " 
-        " UNION ALL SELECT comment.id, comment.parent_id, comment.user, comment.text, comment.date_created, comment.date_modifed, root || ARRAY[comment.id], cte.level + 1" 
-        " FROM cte"
-        " INNER JOIN comment ON comment.parent_id = cte.id)"
-        " SELECT * FROM cte ORDER BY root").params(thread_id=thread_id)
-
-        res = db.engine.execute(stmt)
-        threaded = []
-        for row in res:
-            threaded.append(row)
-            
-        return threaded
+    def get_user(self):
+        return self.user
+    
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- application/threads/views.py
+++ application/threads/views.py
@@ -21,6 +21,7 @@ def get_threads(id):
 @login_required
 def new_thread(id):
     form = ThreadForm(request.form)
+    
     if request.method == "POST" and form.validate():
         t = Thread(form.title.data)
         t.user = current_user.id
@@ -33,6 +34,7 @@ def new_thread(id):
         db.session().add(c)
         db.session().commit()
         return redirect(url_for("read_thread", id=id, thread_id=t.id))
+    
     return render_template("threads/new.html", form=ThreadForm())
 
 
@@ -40,34 +42,14 @@ def new_thread(id):
 def read_thread(id, thread_id):
     form = CommentForm(request.form)
     thread = Thread.query.get(thread_id)
+    
+    comments = Thread.comment_thread(thread)
 
-    cte = db.session.query(Comment).filter_by(thread_id=thread_id)\
-    .filter(Comment.parent_id == None)\
-    .cte(recursive=True)
-    
-    parent = db.aliased(cte)
-    child = db.aliased(Comment)
- 
-    kwery = parent.union_all(db.session.query(child)\
-    .join(parent, child.parent_id == parent.c.id))
-    
-    
-    #comments = db.session.query(kwery).order_by(kwery.c.id).all()
-    
-    comments = Comment.comment_thread(thread_id)
     if request.method == "POST" and form.validate():
         new_comment(thread_id, form)
         return redirect(url_for("read_thread", id=id, thread_id=thread_id))
+    
     return render_template("threads/thread.html", category=id, thread=thread, comments=comments, form=form)
-
-
-#@app.route("/<string:id>/threads/<string:thread_id>/poista")
-#@login_required
-#def delete_thread(id, thread_id):
-#    db.session.query(Comment).filter(Comment.thread_id==thread_id).delete()
-#    db.session.query(Thread).filter(Thread.id==thread_id).delete()
-#    db.session.commit()
-#    return redirect(url_for("get_threads"))
 
     
 @login_required
@@ -82,16 +64,21 @@ def new_comment(thread_id, form):
 @app.route("/<string:comment_id>/edit", methods=["GET", "POST"])
 @login_required
 def edit_comment(comment_id):
-    
+    #TODO: tee tämä
+    if comment.user != current_user.get_id():
+        return login_manager.unauthorized()
+
     form = CommentForm(request.form)
     comment = db.session.query(Comment).filter(Comment.id==comment_id).first()
+    
     if request.method == "POST" and form.validate():
         text = form.text.data
-        db.session.query(Comment).\
-        filter(Comment.id==comment_id).\
-        update({Comment.text: text}, synchronize_session=False)    
+        db.session.query(Comment)\
+        .filter(Comment.id==comment_id)\
+        .update({Comment.text: text}, synchronize_session=False)    
         db.session.commit()
         return redirect(url_for("read_thread", id=id, thread_id=comment.thread_id))
+    
     form.text.data = comment.text
     return render_template("threads/edit.html", form=form, comment=comment)
 
@@ -100,6 +87,7 @@ def edit_comment(comment_id):
 @login_required
 def reply(thread_id, comment_id):
     form = CommentForm(request.form)
+    
     if request.method == "POST" and form.validate():
         c = Comment(form.text.data)
         c.thread_id = thread_id
@@ -108,7 +96,20 @@ def reply(thread_id, comment_id):
         db.session().add(c)
         db.session().commit()
         return redirect(url_for("read_thread", id=id, thread_id=thread_id))
+    
     return render_template("threads/reply.html", form=form)
    
 
+@app.route("/<string:thread_id>/<string:comment_id>/poista", methods=["GET", "POST"])
+@login_required
+def delete_comment(thread_id, comment_id):
+    comment = db.session.query(Comment).filter(Comment.id==comment_id).first()
     
+    #TODO: tee tämä
+    if comment.user != current_user.get_id():
+        return login_manager.unauthorized()
+    
+    comment.text = "Poistettu"
+    comment.is_deleted = True
+    db.session.commit()
+    return redirect(url_for("read_thread", id=id, thread_id=thread_id))
